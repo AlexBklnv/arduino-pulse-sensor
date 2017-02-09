acaf9f08dfa07e404e3bb014ec7b21f57d1c1dae
Alexey <aksel1809@yandex.ru>
2017-02-09 20:50:21 +0900

    Фиксы
    Мелкие правки. Добавление комментариев.

--- RadioHandler.ino
+++ RadioHandler.ino
@@ -3,16 +3,17 @@ const uint64_t pipes[2] = { 0xABCDABCD71, 0x544d52687C };
 void initRadio() {
   radio.begin();
   delay(2);
-  radio.setChannel(91);
+  radio.setChannel(85);
   radio.setDataRate(RF24_1MBPS);
   radio.setPALevel(RF24_PA_MIN);
   radio.setRetries(15, 15);
-  radio.openWritingPipe(pipes[0]);               // Открываем канал передачи
+  radio.openWritingPipe(pipes[0]);                // Открываем канал передачи
   radio.openReadingPipe(1, pipes[1]);             // Открываем канал приема
   radio.startListening();
+    radio.printDetails();
 }
 
-void writeDataToRadio() {
+void writeDataToRadio() {                         // отправка данных на велопк
   if (BPM > 20 && BPM < 300) {
     char msg[3];
     sprintf(msg, "%d", BPM);
@@ -22,7 +23,7 @@ void writeDataToRadio() {
   }
 }
 
-void readCommandFromRadio() {
+void readCommandFromRadio() {                     // прием команд от вело пк
   if (radio.available()) {
     char msg[3];
     radio.read(&msg, 3);
